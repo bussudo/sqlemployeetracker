c0ba06db46e5c6fc5c0e92147d46f310f67e76a0
bussudo <105820926+bussudo@users.noreply.github.com>
2022-08-31 22:18:58 -0400

    made more fixes to index.js

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ function addDepartment() {
       );
     });
 }
+
 function addRole() {
   // connection.query("SELECT * FROM department", function (error, results) {
   //   if (error) {
@@ -154,62 +155,63 @@ function addRole() {
     });
 }
 function addEmployee() {
-  // connection.query("SELECT * from employee", function (error, results) {
-  //   if (error) {
-  //     throw error;
-  //   }
-  //   var empList = results.map((employee) => {
-  //     return {
-  //       name: employee.first_name + " " + employee.last_name,
-  //       value: employee.id,
-  //     };
-  //   });
-  connection.query("SELECT * from role", function (error, results) {
+  connection.query("SELECT * from employee", function (error, results) {
     if (error) {
       throw error;
     }
-    // var roleList = results.map((role) => {
-    //   return {
-    //     name: role.title,
-    //     value: role.id,
-    //   }
-    //       console.log(roleList);
-    //     });
-    //     console.log(empList);
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "firstName",
-          message: "Add the employee first name",
-        },
-        {
-          type: "input",
-          name: "lastName",
-          message: "Add the employee last name",
-        },
-        {
-          type: "list",
-          name: "newRole",
-          message: "Select the role of the new employee",
-          choices: roleList,
-        },
-        {
-          type: "input",
-          name: "newMgr",
-          message: "Select the manager of the new employee if applicable",
-          choices: empList,
-        },
-      ])
-      .then((EmpChoices) => {
-        console.log(
-          EmpChoices.firstName,
-          EmpChoices.lastName,
-          EmpChoices.newRole,
-          EmpChoices.newMgr
-        );
-        menu();
+    var empList = results.map((employee) => {
+      return {
+        name: employee.first_name + " " + employee.last_name,
+        value: employee.id,
+      };
+    });
+    connection.query("SELECT * from role", function (error, results) {
+      if (error) {
+        throw error;
+      }
+      var roleList = results.map((role) => {
+        return {
+          name: role.title,
+          value: role.id,
+        };
+        console.log(roleList);
       });
+      console.log(empList);
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "firstName",
+            message: "Add the employee first name",
+          },
+          {
+            type: "input",
+            name: "lastName",
+            message: "Add the employee last name",
+          },
+          {
+            type: "list",
+            name: "newRole",
+            message: "Select the role of the new employee",
+            choices: roleList,
+          },
+          {
+            type: "input",
+            name: "newMgr",
+            message: "Select the manager of the new employee if applicable",
+            choices: empList,
+          },
+        ])
+        .then((EmpChoices) => {
+          console.log(
+            EmpChoices.firstName,
+            EmpChoices.lastName,
+            EmpChoices.newRole,
+            EmpChoices.newMgr
+          );
+          menu();
+        });
+    });
   });
 }
 
