58ef141e9431e8ae1ed94795b569c1c4939ff195
bussudo <105820926+bussudo@users.noreply.github.com>
2022-09-04 14:43:02 -0400

    updated role function and added ascii-art

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const logo = require("asciiart-logo");
+const config = require("./package.json");
+// console.log(logo(config).render());
+// console.log(logo("Employee Tracker").render());
 const mysql2 = require("mysql2");
 const connection = mysql2.createConnection({
   host: "localhost",
@@ -7,12 +11,16 @@ const connection = mysql2.createConnection({
   password: "M1chig@n",
   database: "employee_db",
 });
+// console.log(logo("Employee Tracker"));
 connection.connect(function (error) {
   if (error) {
     throw error;
   }
 
-  console.log("connected");
+  const title = logo({
+    name: "Employee Tracker",
+  }).render();
+  console.log(title);
   menu();
 });
 function menu() {
@@ -32,8 +40,8 @@ function menu() {
       ],
     })
     .then((answers) => {
-      console.log(answers);
-      console.log(answers.userChoice);
+      // console.log(answers);
+      // console.log(answers.userChoice);
       if (answers.userChoice === "View all Departments") {
         viewDepartments();
       }
@@ -58,7 +66,6 @@ function menu() {
     });
 }
 function viewRoles() {
-  console.log("connection");
   connection.query("SELECT * FROM role", function (error, results) {
     if (error) {
       throw error;
@@ -68,7 +75,6 @@ function viewRoles() {
   });
 }
 function viewDepartments() {
-  console.log("connection");
   connection.query("SELECT * FROM department", function (error, results) {
     if (error) {
       throw error;
@@ -78,7 +84,6 @@ function viewDepartments() {
   });
 }
 function viewEmployees() {
-  console.log("connection");
   connection.query("SELECT * FROM employee", function (error, results) {
     if (error) {
       throw error;
@@ -95,7 +100,6 @@ function addDepartment() {
       message: "What is the name of the department",
     })
     .then((deptChoice) => {
-      console.log(deptChoice.newDept);
       connection.query(
         `INSERT INTO department (name) VALUES ('${deptChoice.newDept}')`,
         function (error, results) {
@@ -110,50 +114,56 @@ function addDepartment() {
 }
 
 function addRole() {
-  // connection.query("SELECT * FROM department", function (error, results) {
-  //   if (error) {
-  //     throw error;
-  //   }
-  //   console.table(results);
-  //   menu();
-  // });
-
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "newTitle",
-        message: "Add the title for the new role",
-      },
-      {
-        type: "input",
-        name: "newSal",
-        message: "Enter the salary for the new role",
-      },
-      {
-        type: "input",
-        name: "newDept",
-        message: "Enter the department for the new role",
-      },
-    ])
-    .then((RoleChoices) => {
-      console.log(
-        RoleChoices.newTitle,
-        RoleChoices.newSal,
-        RoleChoices.newDept
-      ),
-        connection.query(
-          `INSERT INTO role (title, salary, department_id) VALUES ('${RoleChoices.newTitle}',${RoleChoices.newSal},${RoleChoices.newDept})`,
-          function (error, results) {
-            if (error) {
-              throw error;
-            }
-            console.log("Successfully added the new role", results);
-            menu();
-          }
-        );
+  connection.query("SELECT * FROM department", function (error, results) {
+    if (error) {
+      throw error;
+    }
+    var deptList = results.map((department) => {
+      return {
+        name: department.name,
+        value: department.id,
+      };
     });
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "newTitle",
+          message: "Add the title for the new role",
+        },
+        {
+          type: "input",
+          name: "newSal",
+          message: "Enter the salary for the new role",
+        },
+        {
+          type: "list",
+          name: "newDept",
+          message: "Enter the department for the new role",
+          choices: deptList,
+        },
+      ])
+      .then((RoleChoices) => {
+        console.log(
+          RoleChoices.newTitle,
+          RoleChoices.newSal,
+          RoleChoices.newDept
+        ),
+          connection.query(
+            `INSERT INTO role (title, salary, department_id) VALUES ('${RoleChoices.newTitle}',${RoleChoices.newSal},${RoleChoices.newDept})`,
+            function (error, results) {
+              if (error) {
+                throw error;
+              }
+              console.log("Successfully added the new role");
+              menu();
+            }
+          );
+      });
+  });
 }
+
 function addEmployee() {
   connection.query("SELECT * from employee", function (error, results) {
     if (error) {
@@ -174,9 +184,7 @@ function addEmployee() {
           name: role.title,
           value: role.id,
         };
-        console.log(roleList);
       });
-      console.log(empList);
       inquirer
         .prompt([
           {
@@ -252,8 +260,17 @@ function updRole() {
             choices: roleList,
           },
         ])
-        .then((updRole) => {
-          console.log(updRole);
+        .then((answers) => {
+          let updateQuery = `UPDATE employee
+          SET role_id=${answers.updRole}
+          WHERE id=${answers.updName}`;
+          connection.query(updateQuery, (error) => {
+            if (error) {
+              throw error;
+            }
+            console.log("updated successfully");
+            menu();
+          });
         });
     });
   });
